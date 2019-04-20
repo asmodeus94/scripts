68460ca6f97eb4e83612e09bb2c39750067da06b
Adamowicz <r.adamowicz4@wp.pl>
2019-04-20 19:45:20 +0200

    Umozliwienie parsowania wezlow o roznych nazwach, ale bedacych w hierarchii na tym samym poziomie

--- xmlparsing.php
+++ xmlparsing.php
@@ -1,30 +1,43 @@
 <?php
 
+/**
+ * @param string       $filePath
+ * @param string|array $nodeName
+ *
+ * @return Generator
+ * @throws Exception
+ */
 function xmlParsing($filePath, $nodeName)
 {
-    $xmlReader = new XMLReader();
-    if (!$xmlReader->open($filePath)) {
-        return false;
+    $nodesNames = !is_array($nodeName) ? [$nodeName] : $nodeName;
+
+    foreach ($nodesNames as $nodeName) {
+        $xmlReader = new XMLReader();
+        if (!$xmlReader->open($filePath)) {
+            throw new \Exception('An error occurred during xml reading');
+        }
+
+        while ($xmlReader->read() && $xmlReader->name !== $nodeName) {
+        }
+
+        $dom = new DOMDocument();
+
+        while ($xmlReader->name === $nodeName) {
+            $element = simplexml_import_dom($xmlReader->expand($dom));
+            yield $element;
+
+            $xmlReader->next($nodeName);
+        }
+
+        $xmlReader->close();
     }
-
-    while ($xmlReader->read() && $xmlReader->name !== $nodeName) {
-    }
-
-    $dom = new DOMDocument();
-
-    while ($xmlReader->name === $nodeName) {
-        $element = simplexml_import_dom($xmlReader->expand($dom));
-        yield $element;
-
-        $xmlReader->next($nodeName);
-    }
-
-    return $xmlReader->close();
 }
 
 $filePath = '';
 $nodeName = '';
 
 foreach (xmlParsing($filePath, $nodeName) as $index => $node) {
-    // node processing
+    /**
+     * @var SimpleXMLElement $node
+     */
 }
